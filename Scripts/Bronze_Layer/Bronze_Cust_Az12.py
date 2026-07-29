4d97571a75434dadbcd03885e8a3576ce611e00b
Isaac John <isaaczachariahjohn@gmail.com>
2026-07-29 13:27:28 +0100

    Document data loading in Bronze_Cust_Az12.py
    Added comments to explain the data loading process.

--- Scripts/Bronze_Layer/Bronze_Cust_Az12.py
+++ Scripts/Bronze_Layer/Bronze_Cust_Az12.py
@@ -1,1 +1,14 @@
+/*
+--- Loading Cust_AZ12 Data into Bronze Layer
 
+===========================================================
+Reading Data from source and writing to Bronze Delta table
+============================================================
+*/
+
+# read data from source
+df = spark.read.csv(r"/Volumes/workspace/bronze_layer/source_system/Source_crp/CUST_AZ12.csv", header=True, inferSchema=True)
+
+
+# write to Bronze table
+df.write.mode("overwrite").saveAsTable("workspace.bronze_layer.CUST_AZ12")
