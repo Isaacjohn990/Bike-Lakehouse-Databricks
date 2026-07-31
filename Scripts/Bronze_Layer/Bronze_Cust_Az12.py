ffe85dcf0c50680728340be1b3d94203d698ad17
Isaac John <isaaczachariahjohn@gmail.com>
2026-07-31 15:03:11 +0100

    Update Bronze_Cust_Az12.py

--- Scripts/Bronze_Layer/Bronze_Cust_Az12.py
+++ Scripts/Bronze_Layer/Bronze_Cust_Az12.py
@@ -1,10 +1,9 @@
-/*
---- Loading Cust_AZ12 Data into Bronze Layer
 
+
+#LOADING CUST_AZ12 INTO BRONZE LAYER FROM THE SOURCE.
 ===========================================================
 Reading Data from source and writing to Bronze Delta table
 ============================================================
-*/
 
 # read data from source
 df = spark.read.csv(r"/Volumes/workspace/bronze_layer/source_system/Source_crp/CUST_AZ12.csv", header=True, inferSchema=True)
